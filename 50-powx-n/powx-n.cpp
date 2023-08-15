24c2bc11e70c461fe6b355fbb4b525d189c83b69
Deepanshu <121290271+deepanshu186@users.noreply.github.com>
2023-08-15 17:32:09 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (6.81%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+           double ans=1;
+           {
+             if(n>0)
+             {
+               ans=pow(x,n);
+             }
+             else{
+                ans=pow(x,n);
+             }
+           }
+           return ans;
+    }
+};
